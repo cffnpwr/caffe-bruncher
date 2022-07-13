6dc8f1bf9533515d8e7c3200e9bd5ace7d02ccb4
CaffeinePower <yuto1001okuda@gmail.com>
2022-07-13 23:59:47 +0900

    [change] CSSをemotionに移行

---
+++ styles/postForm.ts
@@ -0,0 +1,33 @@
+import { css } from '@emotion/react';
+
+export const postForm = css`
+  width: 30vw;
+`;
+
+export const topbar = css`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const input = css`
+  width: 100%;
+  border: none;
+  background-color: transparent;
+  resize: none;
+  padding: 1em;
+`;
+
+export const inputArea = css`
+  height: 14.5em;
+`;
+export const button = css`
+  border: none;
+  background-color: transparent;
+  padding: 1em;
+`;
+
+export const icon = css`
+  width: 48px;
+  aspect-ratio: 1;
+`;
